ad7d8970a5c7365d78007d0eee083813d36430ed
JarencePlan <106933350+JarencePlan@users.noreply.github.com>
2022-06-05 23:07:54 +0100

    Update UnderstatData.R

--- Code/R/UnderstatData.R
+++ Code/R/UnderstatData.R
@@ -6,3 +6,5 @@ library(understatr)
 data1 = get_player_shots(594) #insert playerID here
 setwd("~/Downloads") #set your desired export location here
 write.csv(data1,"LukakuShot.csv", row.names = FALSE)
+
+#Web Scrapes data from Understat.com, this example gets all the player shot data of a certain player (Lukaku). Saves it to desire folder as a csv to use for other programming scripts.
